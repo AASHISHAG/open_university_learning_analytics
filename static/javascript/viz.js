10d879b7897a1d0c9800e69679270ac80dd20f95
SamehFrihat <samehfrihat@gmail.com>
2020-01-09 14:47:30 +0100

    fix the viz

--- static/javascript/viz.js
+++ static/javascript/viz.js
@@ -45,3 +45,27 @@ var chart = new Chart(ctx, {
     options: {}
 });
 }
+
+
+function testPlot(data_one,data_two, data_three, data_four, data_five){
+
+var ctx = document.getElementById('testChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ["15", "16","17","18","19","20"],
+        datasets: [{
+            label: "Avarage grade based on gender",
+            backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+            borderColor: 'rgb(255, 99, 132)',
+            data: [data_one,data_two, data_three, data_four, data_five,5],
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+}
