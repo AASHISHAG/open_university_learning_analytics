ac319e3219e6fb5d45b7da033e863ba82c434926
AASHISHAG <agarwalaashish20@gmail.com>
2020-01-13 18:34:00 +0100

    Added chi square test results and visualisation for decision tree

--- prediction.py
+++ prediction.py
@@ -4,7 +4,23 @@ import numpy as np
 X_test = np.array([118,0,18,0,83,5,0,1,12,0,3,0,2,15,0,5,0,202]).reshape(1, -1)
 
 # loading the model from disk
-loaded_model = pickle.load(open('random_forest_model.sav', 'rb'))
-result = loaded_model.predict(X_test)
-print(result)
+#get_model()
+#loaded_model = pickle.load(open('random_forest_model.sav', 'rb'))
+#result = loaded_model.predict(X_test)
+#print(result)
+
+def getModel(model_name):
+    if model_name == 'decision-tree':
+        model = 'decision_tree_model.sav'
+    elif model_name == 'gradient-boosting':
+        model = 'gradient_boosting_model.sav'
+    elif model_name == 'random-forest':
+        model = 'random_forest_model.sav'
+
+    return pickle.load(open(model, 'rb'))
+
+def getPrediction(model_name):
+    model = getModel(model_name)
+    grade = model.predict(X_test)
+    print("The predicted grade is: {}".format(grade[0]))
 
