a32f99f7897fb996f7f73304d5b04c90c6f0bb1c
Sameh Frihat <sameh.frihat@stud.uni-due.de>
2020-01-21 20:21:34 +0100

    add more graphs

--- static/javascript/viz.js
+++ static/javascript/viz.js
@@ -1,41 +1,3 @@
-//each chart has been plotted with each function in template part
-
-function genderBar(){
-    var ctx = document.getElementById('genderBar').getContext('2d');
-			window.myBar = new Chart(ctx, {
-				type: 'bar',
-				data: {
-			labels: ['January', 'February', 'March', 'April'],
-			datasets: [{
-				label: 'Male',
-				backgroundColor: color(window.chartColors.red).alpha(0.5).rgbString(),
-				borderColor: window.chartColors.red,
-				borderWidth: 1,
-				data: [ 858, 6240, 2909, 20332]
-			}, {
-				label: 'Female',
-				backgroundColor: color(window.chartColors.blue).alpha(0.5).rgbString(),
-				borderColor: window.chartColors.blue,
-				borderWidth: 1,
-				data: [ 345, 2403, 1497, 11826]
-			}]
-
-		},
-				options: {
-					responsive: true,
-					legend: {
-						position: 'top',
-					},
-					title: {
-						display: true,
-						text: 'Chart.js Bar Chart'
-					}
-				}
-			});
-
-}
-
-
 //https://www.chartjs.org/docs/latest/getting-started/
 function genderPlot(data_one,data_two){
 
@@ -108,6 +70,90 @@ var chart = new Chart(ctx, {
             borderColor: 'rgb(255, 99, 132)',
             data: [data_one,data_two, data_three, data_four, data_five,5],
         }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+}
+
+
+
+
+function treePlot(data_one,data_two){
+
+var ctx = document.getElementById('featursImportance').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'horizontalBar',
+    // The data for our dataset
+    data: {
+        datasets: [{
+    label: "Gender",
+    backgroundColor: ["#3e95cd"],
+    borderColor: 'rgb(255, 99, 132)',
+    data: [1.346817814],
+},
+{
+    label: "Age Band",
+    backgroundColor: ["#8e5ea2"],
+    borderColor: 'rgb(255, 99, 132)',
+    data: [0.262031797],
+},
+{
+    label: "Semester",
+    backgroundColor: ["#3cba9f"],
+    borderColor: 'rgb(255, 99, 132)',
+    data: [0.19188812],
+},
+{
+    label: "Region",
+    backgroundColor: ["#e8c3b9"],
+    borderColor: 'rgb(255, 99, 132)',
+    data: [0.179155034],
+},
+{
+	label: "Semester (First Module)",
+	backgroundColor: ["#c45850"],
+	borderColor: 'rgb(255, 99, 132)',
+	data: [0.155225322],
+},
+{
+	label: "Highest Education",
+	backgroundColor: ["#fe9dcd"],
+	borderColor: 'rgb(255, 99, 132)',
+	data: [0.118247178],
+},
+{
+	label: "Imd Band",
+	backgroundColor: ["#8a5ea2"],
+	borderColor: 'rgb(255, 99, 132)',
+	data: [0.11134633],
+},
+{
+	label: "First Module",
+	backgroundColor: ["#300a9f"],
+	borderColor: 'rgb(255, 99, 132)',
+	data: [0.081328867],
+},
+{
+	label: "Second Module",
+	backgroundColor: ["#7883b9"],
+	borderColor: 'rgb(255, 99, 132)',
+	data: [0.059469076],
+},
+{
+	label: "Number Of Previous Attempts",
+	backgroundColor: ["#235850"],
+	borderColor: 'rgb(255, 99, 132)',
+	data: [0.042821206],
+},
+{
+	label: "Semester (Second Module)",
+	backgroundColor: ["#b45850"],
+	borderColor: 'rgb(255, 99, 132)',
+	data: [0.038979394],
+}]
     },
 
     // Configuration options go here
