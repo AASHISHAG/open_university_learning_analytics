0ca3fef5a77ff253b4bc0f6589114d3636fc34aa
AASHISHAG <agarwalaashish20@gmail.com>
2020-01-26 20:03:04 +0100

    updated format on aboutus and index page

--- server.py
+++ server.py
@@ -271,7 +271,7 @@ def Real_time_statistics():
     return render_template('Real_time_statistics.html',
                            male=male, female=female, w_male=w_male, w_female=w_female,
                            p_male=p_male, p_female=p_female, f_male=f_male, f_female=f_female,
-                           d_male=d_male, d_female=d_female, title='Real time statistics')
+                           d_male=d_male, d_female=d_female, title='Real Time Statistics')
 
 # main function
 if __name__ == "__main__":
